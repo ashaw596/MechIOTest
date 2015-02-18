05291e3212a24afb4d290cd1fb6e33babeda4fc0
ashaw596 <ashaw596@gmail.com>
2015-02-18 17:30:09 -0500

    Add Emotion Animations.

--- Java/mechioTest/src/main/java/com/mycompany/mechiotest/main.java
+++ Java/mechioTest/src/main/java/com/mycompany/mechiotest/main.java
@@ -7,6 +7,9 @@
 package com.mycompany.mechiotest;
 
 import org.jflux.api.common.rk.position.NormalizedDouble;
+import org.mechio.api.animation.Animation;
+import org.mechio.api.animation.messaging.RemoteAnimationPlayerClient;
+import org.mechio.api.animation.player.AnimationJob;
 import org.mechio.api.motion.*;
 import org.mechio.api.motion.Robot.*;
 import org.mechio.api.motion.messaging.RemoteRobot;
@@ -19,6 +22,12 @@ import org.mechio.client.basic.*;
 public class main {
     
     private RemoteRobot robot;
+    private RemoteAnimationPlayerClient animPlayer;
+    private Animation happyAnim;
+    private Animation sadAnim;
+    private Animation panicAnim;
+    private Animation surpriseAnim;
+    private Animation angryAnim;
     public main (String robotIP) {
         System.out.println("Constructor("+robotIP+")");
         UserSettings.setRobotAddress(robotIP);
@@ -26,7 +35,18 @@ public class main {
     public void connectRobot() {
         System.out.println("Connect Robot");
         robot = MechIO.connectRobot();
+        animPlayer = MechIO.connectAnimationPlayer();
+        loadAnims();
     }
+    
+    public void loadAnims() {
+        happyAnim = MechIO.loadAnimation("animations/AZR25_happy_01.anim.xml"); 
+        sadAnim = MechIO.loadAnimation("animations/AZR25_sad_01.anim.xml");
+        surpriseAnim = MechIO.loadAnimation("animations/AZR25_surprise_01.anim.xml");
+        angryAnim = MechIO.loadAnimation("animations/AZR25_angry_01.anim.xml");
+        panicAnim = MechIO.loadAnimation("animations/AZR25_panic_01.anim.xml");
+    }
+    
     public void moveNeckYaw(double goalPosition, int time) {
         JointId neckYaw = new JointId(robot.getRobotId(), new Joint.Id(R50RobotJoints.NECK_YAW));
         RobotPositionMap goalPositions = new RobotPositionHashMap();
@@ -57,6 +77,54 @@ public class main {
             MechIO.sleep(1000);
         }
     }
+    
+    public AnimationJob playHappyAnim() {
+        return playAnimation(happyAnim);
+    }
+    public AnimationJob playSadAnim() {
+        return playAnimation(sadAnim);
+    }
+    public AnimationJob playPanicAnim() {
+        return playAnimation(panicAnim);
+    }
+    public AnimationJob playAngryAnim() {
+        return playAnimation(angryAnim);
+    }
+    public AnimationJob playSurpriseAnim() {
+        return playAnimation(surpriseAnim);
+    }
+    
+    public AnimationJob playAnimation(Animation anim) {
+        return animPlayer.playAnimation(anim);
+    }
+
     public static void main(String args[]) {
+        main robot = new main("127.0.0.1");
+        robot.connectRobot();
+        System.out.println("connected");
+        while (true) {
+            System.out.println("Happy");
+            AnimationJob job = robot.playHappyAnim();
+            MechIO.sleep(500 + job.getAnimationLength());
+            System.out.println("Sad");
+            job = robot.playSadAnim();
+            MechIO.sleep(500 + job.getAnimationLength());
+            System.out.println("Angry");
+            job = robot.playAngryAnim();
+            MechIO.sleep(500 + job.getAnimationLength());
+            
+            System.out.println("Panic");
+            job = robot.playPanicAnim();
+            MechIO.sleep(500 + job.getAnimationLength());
+            System.out.println("Surprise");
+            job = robot.playSurpriseAnim();
+            MechIO.sleep(500 + job.getAnimationLength());
+            //robot.moveNeckYaw(.2, 1000);
+           // MechIO.sleep(1000);
+            //ro^ot.moveNeckYaw(.8, 1000);
+            //MechIO.sleep(1000);
+            
+            
+        }
     }
 }
